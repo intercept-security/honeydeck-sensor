dc8f5c0d8ef8ec6d7890a257a69385eed3ca44de
david <david@misplaced.us>
2020-09-10 20:19:21 -0600

    updates to install file

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
-sudo yum install python3 -y
-mkdir .venvs
-python3 -m venv .venvs/honeydeck_sensor
-sudo pip install requirements.txt
+
+# Add github's ssh fingerprints
+if ! grep -q "^github.com" ~/.ssh/known_hosts; then
+    ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
+fi
+
+# Install required packages
+sudo yum install python3 git -y
+
+# Install required pip packages
+sudo pip3 -r install requirements.txt
