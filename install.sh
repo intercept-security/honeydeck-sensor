4ca83bf58154dcf1e0f39e4af748a90cdf7dcb6b
david <david@misplaced.us>
2020-09-11 22:11:59 -0600

    fix cron bug

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+###### Honeydeck Sensor Install Script ######
+
 # Check for root before beginning
 if [ "$EUID" -ne 0 ]
     then echo "This installer must be run as root."
@@ -13,25 +15,25 @@ CURRENT_BRANCH="$(git branch)"
 
 mkdir -p ${LOG_FILE}
 
-echo "### $(date) Performing Update ###" | tee ${LOG_FILE}
-echo "UPDATE_INTERVAL: ${UPDATE_INTERVAL}" | tee ${LOG_FILE}
-echo "UPDATER_PATH: ${UPDATER_PATH}" | tee ${LOG_FILE}
-echo "CURRENT_BRANCH: ${CURRENT_BRANCH}" | tee ${LOG_FILE}
+echo "###### $(date) Performing Update ######" | tee ${LOG_FILE}
+echo "### UPDATE_INTERVAL: ${UPDATE_INTERVAL}" | tee ${LOG_FILE}
+echo "### UPDATER_PATH: ${UPDATER_PATH}" | tee ${LOG_FILE}
+echo "### CURRENT_BRANCH: ${CURRENT_BRANCH}" | tee ${LOG_FILE}
 
-echo "Installing Required Packages" | tee ${LOG_FILE}
+echo "### Installing Required Packages" | tee ${LOG_FILE}
 yum install python3 git -y
 
-echo "Adding github's ssh fingerprints" | tee ${LOG_FILE}
+echo "### Adding github's ssh fingerprints" | tee ${LOG_FILE}
 if ! grep -q "^github.com" ~/.ssh/known_hosts; then
     ssh-keyscan -t rsa github.com tee ~/.ssh/known_hosts
 fi
 
-echo "Fetching latest version" | tee ${LOG_FILE}
+echo "### Fetching latest version" | tee ${LOG_FILE}
 git pull
 
-echo "Installing required pip packages" | tee ${LOG_FILE}
+echo "### Installing required pip packages" | tee ${LOG_FILE}
 pip3 install -r requirements.txt
 
-echo "Adding cron updater" | tee ${LOG_FILE}
+echo "### Adding cron updater" | tee ${LOG_FILE}
 grep "sh ${UPDATER_PATH}" /etc/crontab || \
-    echo "*/${UPDATE_INTERVAL} *  *  *  * sh honeydeck-sensor/update.sh" tee /etc/crontab
+    echo "*/${UPDATE_INTERVAL} *  *  *  * sh honeydeck-sensor/update.sh" >> /etc/crontab
