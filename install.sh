6093331b21da77f6452701ec5c067789e4ef0ea3
david <david@misplaced.us>
2020-09-11 22:21:11 -0600

    fix logfile bug

--- install.sh
+++ install.sh
@@ -9,29 +9,29 @@ UPDATER_PATH="$(dirname $(realpath ${0}))/update.sh"
 CURRENT_BRANCH="$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')"
 
 sudo mkdir -p ${LOG_DIR}
-sudo touch ${LOG_FIL}}
+sudo touch ${LOG_FILE}}
 
-sudo echo "###### $(date) Performing Install ######" | tee ${LOG_FILE}
-sudo echo "### UPDATE_INTERVAL: ${UPDATE_INTERVAL}" | tee ${LOG_FILE}
-sudo echo "### UPDATER_PATH: ${UPDATER_PATH}" | tee ${LOG_FILE}
-sudo echo "### CURRENT_BRANCH: ${CURRENT_BRANCH}" | tee ${LOG_FILE}
+sudo echo "###### $(date) Performing Install ######" | sudo tee ${LOG_FILE}
+sudo echo "### UPDATE_INTERVAL: ${UPDATE_INTERVAL}" | sudo tee ${LOG_FILE}
+sudo echo "### UPDATER_PATH: ${UPDATER_PATH}" | sudo tee ${LOG_FILE}
+sudo echo "### CURRENT_BRANCH: ${CURRENT_BRANCH}" | sudo tee ${LOG_FILE}
 
-sudo echo "### Installing Required Packages" | tee ${LOG_FILE}
+sudo echo "### Installing Required Packages" | sudo tee ${LOG_FILE}
 sudo yum install python3 git -y
 
-sudo echo "### Adding github's ssh fingerprints" | tee ${LOG_FILE}
+sudo echo "### Adding github's ssh fingerprints" | sudo tee ${LOG_FILE}
 if ! grep -q "^github.com" ~/.ssh/known_hosts; then
     ssh-keyscan -t rsa github.com tee ~/.ssh/known_hosts
 fi
 
-sudo echo "### Fetching latest version from ${CURRENT_BRANCH}" | tee ${LOG_FILE}
+sudo echo "### Fetching latest version from ${CURRENT_BRANCH}" | sudo tee ${LOG_FILE}
 git pull
 
-sudo echo "### Installing required pip packages" | tee ${LOG_FILE}
+sudo echo "### Installing required pip packages" | sudo tee ${LOG_FILE}
 pip3 install -r requirements.txt
 
-sudo echo "### Adding cron updater" | tee ${LOG_FILE}
+sudo echo "### Adding cron updater" | sudo tee ${LOG_FILE}
 grep "sh ${UPDATER_PATH}" /etc/crontab || \
     sudo echo "*/${UPDATE_INTERVAL} *  *  *  * sh honeydeck-sensor/update.sh" >> /etc/crontab
 
-sudo echo "###### $(date) Completed Install ######" | tee ${LOG_FILE}
+sudo echo "###### $(date) Completed Install ######" | sudo tee ${LOG_FILE}
