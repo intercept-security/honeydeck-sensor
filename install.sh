15e2b1668b84533285a00ba1ae9d56ebf85672f4
david <david@misplaced.us>
2020-09-11 22:18:29 -0600

    fix git branch

--- install.sh
+++ install.sh
@@ -2,38 +2,36 @@
 
 ###### Honeydeck Sensor Install Script ######
 
-# Check for root before beginning
-if [ "$EUID" -ne 0 ]
-    then echo "This installer must be run as root."
-    exit
-fi
-
-LOG_FILE=/var/log/honeydeck/sensor/log.txt
+LOG_DIR=/var/log/honeydeck/sensor
+LOG_FILE="${LOG_DIR}/log.txt"
 UPDATE_INTERVAL=15 # minutes
 UPDATER_PATH="$(dirname $(realpath ${0}))/update.sh"
-CURRENT_BRANCH="$(git branch)"
+CURRENT_BRANCH="$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')"
 
-mkdir -p ${LOG_FILE}
+sudo mkdir -p ${LOG_DIR}
+sudo touch ${LOG_FIL}}
 
-echo "###### $(date) Performing Update ######" | tee ${LOG_FILE}
-echo "### UPDATE_INTERVAL: ${UPDATE_INTERVAL}" | tee ${LOG_FILE}
-echo "### UPDATER_PATH: ${UPDATER_PATH}" | tee ${LOG_FILE}
-echo "### CURRENT_BRANCH: ${CURRENT_BRANCH}" | tee ${LOG_FILE}
+sudo echo "###### $(date) Performing Install ######" | tee ${LOG_FILE}
+sudo echo "### UPDATE_INTERVAL: ${UPDATE_INTERVAL}" | tee ${LOG_FILE}
+sudo echo "### UPDATER_PATH: ${UPDATER_PATH}" | tee ${LOG_FILE}
+sudo echo "### CURRENT_BRANCH: ${CURRENT_BRANCH}" | tee ${LOG_FILE}
 
-echo "### Installing Required Packages" | tee ${LOG_FILE}
-yum install python3 git -y
+sudo echo "### Installing Required Packages" | tee ${LOG_FILE}
+sudo yum install python3 git -y
 
-echo "### Adding github's ssh fingerprints" | tee ${LOG_FILE}
+sudo echo "### Adding github's ssh fingerprints" | tee ${LOG_FILE}
 if ! grep -q "^github.com" ~/.ssh/known_hosts; then
     ssh-keyscan -t rsa github.com tee ~/.ssh/known_hosts
 fi
 
-echo "### Fetching latest version" | tee ${LOG_FILE}
+sudo echo "### Fetching latest version from ${CURRENT_BRANCH}" | tee ${LOG_FILE}
 git pull
 
-echo "### Installing required pip packages" | tee ${LOG_FILE}
+sudo echo "### Installing required pip packages" | tee ${LOG_FILE}
 pip3 install -r requirements.txt
 
-echo "### Adding cron updater" | tee ${LOG_FILE}
+sudo echo "### Adding cron updater" | tee ${LOG_FILE}
 grep "sh ${UPDATER_PATH}" /etc/crontab || \
-    echo "*/${UPDATE_INTERVAL} *  *  *  * sh honeydeck-sensor/update.sh" >> /etc/crontab
+    sudo echo "*/${UPDATE_INTERVAL} *  *  *  * sh honeydeck-sensor/update.sh" >> /etc/crontab
+
+sudo echo "###### $(date) Completed Install ######" | tee ${LOG_FILE}
