71e7dde5052003160c09a1087401b7f8c9b5278b
david <david@misplaced.us>
2020-09-11 22:18:43 -0600

    add comments to update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,23 @@
 #!/bin/sh
+
+###### Honeydeck Sensor Install Script ######
+
+CURRENT_BRANCH="$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')"
+LOG_DIR=/var/log/honeydeck/sensor
+LOG_FILE="${LOG_DIR}/log.txt"
+
+sudo mkdir -p ${LOG_FILE}
+sudo touch ${LOG_FILE}
+
+sudo echo "###### $(date) Performing Update ######" | tee ${LOG_FILE}
+sudo echo "### CURRENT_BRANCH: ${CURRENT_BRANCH}" | tee ${LOG_FILE}
+
+
+sudo echo "### Fetching latest version from ${CURRENT_BRANCH}" | tee ${LOG_FILE}
 git pull
+
+sudo echo "### Deploying updater playbook" | tee ${LOG_FILE}
 cd sensor
 ansible-playbook main.yml
+
+sudo echo "###### $(date) Completed Update ######" | tee ${LOG_FILE}
