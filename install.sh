9d84220f1c2e7799936f643d4cf5952537922546
david <david@misplaced.us>
2020-09-11 22:26:26 -0600

    fix tee append bug

--- install.sh
+++ install.sh
@@ -11,27 +11,27 @@ CURRENT_BRANCH="$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')"
 sudo mkdir -p ${LOG_DIR}
 sudo touch ${LOG_FILE}}
 
-sudo echo "###### $(date) Performing Install ######" | sudo tee ${LOG_FILE}
-sudo echo "### UPDATE_INTERVAL: ${UPDATE_INTERVAL}" | sudo tee ${LOG_FILE}
-sudo echo "### UPDATER_PATH: ${UPDATER_PATH}" | sudo tee ${LOG_FILE}
-sudo echo "### CURRENT_BRANCH: ${CURRENT_BRANCH}" | sudo tee ${LOG_FILE}
+sudo echo "###### $(date) Performing Install ######" | sudo tee -a ${LOG_FILE}
+sudo echo "### UPDATE_INTERVAL: ${UPDATE_INTERVAL}" | sudo tee -a ${LOG_FILE}
+sudo echo "### UPDATER_PATH: ${UPDATER_PATH}" | sudo tee -a ${LOG_FILE}
+sudo echo "### CURRENT_BRANCH: ${CURRENT_BRANCH}" | sudo tee -a ${LOG_FILE}
 
-sudo echo "### Installing Required Packages" | sudo tee ${LOG_FILE}
+sudo echo "### Installing Required Packages" | sudo tee -a ${LOG_FILE}
 sudo yum install python3 git -y
 
-sudo echo "### Adding github's ssh fingerprints" | sudo tee ${LOG_FILE}
+sudo echo "### Adding github's ssh fingerprints" | sudo tee -a ${LOG_FILE}
 if ! grep -q "^github.com" ~/.ssh/known_hosts; then
-    ssh-keyscan -t rsa github.com tee ~/.ssh/known_hosts
+    ssh-keyscan -t rsa github.com tee -a ~/.ssh/known_hosts
 fi
 
-sudo echo "### Fetching latest version from ${CURRENT_BRANCH}" | sudo tee ${LOG_FILE}
+sudo echo "### Fetching latest version from ${CURRENT_BRANCH}" | sudo tee -a ${LOG_FILE}
 git pull
 
-sudo echo "### Installing required pip packages" | sudo tee ${LOG_FILE}
+sudo echo "### Installing required pip packages" | sudo tee -a ${LOG_FILE}
 pip3 install -r requirements.txt
 
-sudo echo "### Adding cron updater" | sudo tee ${LOG_FILE}
+sudo echo "### Adding cron updater" | sudo tee -a ${LOG_FILE}
 sudo grep "sh ${UPDATER_PATH}" /etc/crontab || \
-    sudo echo "*/${UPDATE_INTERVAL} *  *  *  * sh honeydeck-sensor/update.sh" >> /etc/crontab
+    sudo echo "*/${UPDATE_INTERVAL} *  *  *  * sh honeydeck-sensor/update.sh" | sudo tee -a /etc/crontab
 
-sudo echo "###### $(date) Completed Install ######" | sudo tee ${LOG_FILE}
+sudo echo "###### $(date) Completed Install ######" | sudo tee -a ${LOG_FILE}
--- update.sh
+++ update.sh
@@ -9,15 +9,15 @@ LOG_FILE="${LOG_DIR}/log.txt"
 sudo mkdir -p ${LOG_FILE}
 sudo touch ${LOG_FILE}
 
-sudo echo "###### $(date) Performing Update ######" | sudo tee ${LOG_FILE}
-sudo echo "### CURRENT_BRANCH: ${CURRENT_BRANCH}" | sudo tee ${LOG_FILE}
+sudo echo "###### $(date) Performing Update ######" | sudo tee -a ${LOG_FILE}
+sudo echo "### CURRENT_BRANCH: ${CURRENT_BRANCH}" | sudo tee -a ${LOG_FILE}
 
 
-sudo echo "### Fetching latest version from ${CURRENT_BRANCH}" | sudo tee ${LOG_FILE}
+sudo echo "### Fetching latest version from ${CURRENT_BRANCH}" | sudo tee -a ${LOG_FILE}
 git pull
 
-sudo echo "### Deploying updater playbook" | sudo tee ${LOG_FILE}
+sudo echo "### Deploying updater playbook" | sudo tee -a ${LOG_FILE}
 cd sensor
 ansible-playbook main.yml
 
-sudo echo "###### $(date) Completed Update ######" | sudo tee ${LOG_FILE}
+sudo echo "###### $(date) Completed Update ######" | sudo tee -a ${LOG_FILE}
