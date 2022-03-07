1c6095635417a9f453c153e25e7a79dcb9a28358
Jordan Brown <jordan@jobro.me>
2022-03-06 20:34:38 -0700

    fix typo, ensure jq exists, update user-data script

--- contrib/userdata-amz2.sh
+++ contrib/userdata-amz2.sh
@@ -10,15 +10,23 @@
 HONEYDECK_SERVER="< honeydeck_server_url_goes_here >"
 SENSOR_GROUP_TOKEN="< sensor_group_token_goes_here >"
 
-exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
-yum update -y --skip-broken
-yum install git jq -y --skip-broken
-su - ec2-user -c "cd ~ec2-user/ && git clone https://github.com/intercept-security/honeydeck-sensor.git"
-su - ec2-user -c "cd ~ec2-user/honeydeck-sensor && ./install.sh"
-su - ec2-user -c "cd ~ec2-user/honeydeck-sensor && cp honeydeck-sensor-sample.yml honeydeck-sensor.yml"
+amazon-linux-extras install epel -y
+
+yum update -y
+yum upgrade -y
+
+yum -y install fail2ban git jq
+systemctl enable fail2ban
+systemctl start fail2ban
+
+su - ec2-user -c "cd ~ec2-user && git clone https://github.com/intercept-security/honeydeck-sensor.git"
+
 instance_id=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
-su - ec2-user -c "cd ~ec2-user/honeydeck-sensor &&  python3 util/update_honeydeck_config.py honeydeck-sensor.yml sensor_name $instance_id"
-su - ec2-user -c "cd ~ec2-user/honeydeck-sensor && python3 util/update_honeydeck_config.py honeydeck-sensor.yml sensor_group_token $SENSOR_GROUP_TOKEN"
-su - ec2-user -c "cd ~ec2-user/honeydeck-sensor && python3 util/update_honeydeck_config.py honeydeck-sensor.yml honeydeck_server $HONEYDECK_SERVER"
+su - ec2-user -c "cd ~ec2-user/honeydeck-sensor && echo 'sensor_name: $instance_id' >> honeydeck-sensor.yml"
+
+su - ec2-user -c "cd ~ec2-user/honeydeck-sensor && echo 'honeydeck_server: $HONEYDECK_SERVER' >> honeydeck-sensor.yml"
+su - ec2-user -c "cd ~ec2-user/honeydeck-sensor && echo 'sensor_group_token: $SENSOR_GROUP_TOKEN' >> honeydeck-sensor.yml"
+su - ec2-user -c "cd ~ec2-user/honeydeck-sensor && echo 'sensor_roles: [ \"ssh_honeypot\" ]' >> honeydeck-sensor.yml"
+
+su - ec2-user -c "cd ~ec2-user/honeydeck-sensor && ./install.sh"
 su - ec2-user -c "cd ~ec2-user/honeydeck-sensor && ./register_sensor.sh"
-su - ec2-user -c "cd ~ec2-user/honeydeck-sensor && ./update.sh"
