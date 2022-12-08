3d30575f4a939590e57bd6d0468301aca71895f3
David Anderson <david@misplaced.us>
2022-12-07 17:25:01 -0700

    upgrade pip before installing ansible

--- sensor/install_debian.sh
+++ sensor/install_debian.sh
@@ -3,6 +3,7 @@ sudo apt-get update
 sudo apt-get install python3 git python3-pip -y
 sudo apt install python3.10-venv -y
 
+pip3 install --user --upgrade pip
 pip3 install ansible --user
 sudo apt-get -v &> /dev/null
 export PATH="$PATH:$HOME/.local/bin"
